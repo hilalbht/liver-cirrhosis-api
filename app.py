1fd8711bfe1d713358b0768a3a4c6cdcd2830c7a
Hilal <hilal.bht34@gmail.com>
2026-01-01 18:05:29 +0300

    Updated app.py and requirements.txt for Render

--- app.py
+++ app.py
@@ -7,11 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-# Eğer başka route’lar ekleyeceksen buraya ekleyebilirsin
-
 if __name__ == "__main__":
-    # Render deploy sırasında PORT ortam değişkeni veriyor, yoksa local için 5000
     port = int(os.environ.get("PORT", 5000))
-    # host=0.0.0.0 ile dış dünyaya açıyoruz
-    # debug=True ile geliştirme modunu aktif tutuyoruz
     app.run(host="0.0.0.0", port=port, debug=True)
